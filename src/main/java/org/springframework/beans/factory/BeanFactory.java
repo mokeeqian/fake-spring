c0b33cfc86219b6cfd1a9959ae064449eb003d12
mokeeqian <mokeeqian@gmail.com>
2022-06-02 21:43:19 +0800

    1 实现简单的bean容器

--- src/main/java/org/springframework/beans/factory/BeanFactory.java
+++ src/main/java/org/springframework/beans/factory/BeanFactory.java
@@ -7,14 +7,27 @@ package org.springframework.beans.factory;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 简单的 bean 容器，使用 hashmap 来实现
+ */
 public class BeanFactory {
     // bean 容器
     private Map<String, Object> beanMap = new HashMap<>();
 
+    /**
+     * bean 注册
+     * @param name bean名称，作为key放入hashmap
+     * @param bean bean对象，作为value存入hashmap
+     */
     public void registerBean(String name, Object bean) {
         beanMap.put(name, bean);
     }
 
+    /**
+     * 按照名称获取bean对象
+     * @param name 名称
+     * @return bean对象
+     */
     public Object getBean(String name) {
         return beanMap.get(name);
     }
